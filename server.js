834909c09e52c9206a045eb68c4703700f3118a2
Renjith <renjithrrkj@gmail.com>
2017-05-07 11:57:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/', function (req, res) {
+app.get('/ui/madi.png', function (req, res) {
   res.sendFile('http://www.carlogos.org/logo/Mercedes-Benz-logo.jpg');
 });
 
